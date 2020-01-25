204d6b7a041f505068effea0c271619e849fbea7
anjelikasah <anjelikasah98@gmail.com>
2020-01-25 23:42:18 +0545

    Working AudioNet on present changes

--- scripts/data_maker.py
+++ scripts/data_maker.py
@@ -23,7 +23,9 @@ def mp3towav(path):
           wa = file.replace('mp3', 'wav')
           try:
             print("Converting using sox")
-            subprocess.call(['sox', mp, '-e', 'mu-law', '-r', '16k', wa, 'remix', '1,2'])
+            sound = pydub.AudioSegment.from_mp3(mp)
+            sound.export(wa, format="wav")
+            # subprocess.call(['sox', mp, '-e', 'mu-law', '-r', '16k', wa, 'remix', '1,2'])
           except Exception as e:
             print("Converting using ffmpeg")
             try:
@@ -35,7 +37,7 @@ def makechunks(path):
     folders=glob.glob(path+'*')
     for folder in folders:
       waves = glob.glob(folder+'/'+ '*.wav')
-      print 'w',waves
+      print ('w',waves)
       if len(waves) == 0:
           return 10
       for i in waves:
@@ -43,27 +45,27 @@ def makechunks(path):
           myaudio = AudioSegment.from_file(i, 'wav')
           chunk_length_ms = 20000
           chunks = make_chunks(myaudio, chunk_length_ms)
-          print chunks
+          print (chunks)
           for i, chunk in enumerate(chunks):
               chunk_name = w.split('.')[0] + "chunk{0}.wav".format(i)
-              print chunk_name
-              print "exporting", chunk_name
+              print (chunk_name)
+              print ("exporting", chunk_name)
               chunk.export(folder+'/'+chunk_name, format="wav")
 
 
 def graph_spectrogram(wav_file):
     rate, data = get_wav_info(wav_file)
-    print type(data), len(data)
+    print (type(data), len(data))
     nfft = 256  # Length of the windowing segments
     fs = 256  # Sampling frequency
     pxx, freqs, bins, im = plt.specgram(data, nfft, fs)
-    print "pxx : ", len(pxx)
-    print "freqs : ", len(freqs)
-    print "bins : ", len(bins)
+    print ("pxx : ", len(pxx))
+    print ("freqs : ", len(freqs))
+    print ("bins : ", len(bins))
     # plt.axis('on')
     # plt.show()
     plt.axis('off')
-    print wav_file.split('.wav')[0]
+    print (wav_file.split('.wav')[0])
     plt.savefig(wav_file.split('.wav')[0] + '.png',
                 dpi=100,  # Dots per inch
                 frameon='false',
@@ -75,7 +77,7 @@ def graph_spectrogram(wav_file):
       rgb_im = im.convert('RGB')
       rgb_im.save(wav_file.split('.png')[0] + '.jpg')
     except Exception as e:
-      print e
+      print (e)
     if os.path.exists(wav_file.split('.wav')[0] + '.png'):
         os.system('convert '+(wav_file.split('.wav')[0] + '.png') + ' '+(wav_file.split('.wav')[0] + '.jpg'))
         os.remove(wav_file.split('.wav')[0] + '.png')
@@ -90,22 +92,22 @@ def wav2spectrogram(path):
     folders = glob.glob(path+'*')
     for folder in folders:
       waves = glob.glob(folder+'/' + '*.wav')
-      print waves
+      print (waves)
       if len(waves) == 0:
         return 10
       for f in waves:
         try:
-            print "Generating spectrograms.."
+            print ("Generating spectrograms..")
             graph_spectrogram(f)
         except Exception as e:
-            print "Something went wrong while generating spectrogram: ", e
+            print ("Something went wrong while generating spectrogram: ", e)
 
 def move_images(path):
     folders = glob.glob('*')
     for folder in folders:
         os.makedirs('../tf_files/data_image/'+folder)
         waves=glob.glob('*.jpg')
-        print waves
+        print (waves)
         for wav in waves:
             shutil.move(path+folder+'/'+wav,'../tf_files/data_image/'+folder+'/'+wav)
 
@@ -119,42 +121,34 @@ if __name__ == '__main__':
     parser.add_argument('--spectrogram', help="Set this flags  to create spectrograms", action="store_true",default=True)
     args = parser.parse_args()
     if args.mp3towav:
-        print
-        "Path : ", path
+        print ("Path : ", path)
         try:
             r = mp3towav(path)
             if r == 10:
-                print
-                "No mp3 files in specified directory"
+                print ("No mp3 files in specified directory")
             else:
-                print
-                "All mp3 files processed completely"
+                print ("All mp3 files processed completely")
         except Exception as e:
-            print
-            "Something went wrong :", e
+            print ("Something went wrong :", e)
     if args.mkchunks:
-        print
-        "Searching for wav files in :", path
+        print ("Searching for wav files in :", path)
         try:
             r = makechunks(path)
             if r == 10:
-                print
-                "No wav files in given path"
+                print ("No wav files in given path")
             else:
-                print
-                "Completed successfully"
+                print ("Completed successfully")
         except Exception as e:
-            print
-            "Something went wrong : ", e
+            print ("Something went wrong : ", e)
     if args.spectrogram:
-        print "Finding files in : ", path
+        print ("Finding files in : ", path)
         try:
             r = wav2spectrogram(path)
             if r == 10:
-                print "No wav files found in the given path"
+                print ("No wav files found in the given path")
             else:
                 move_images(path)
-                print "All mp3 files processed completely"
+                print ("All mp3 files processed completely")
         except Exception as e:
             move_images(path)
-            print "Something went wrong: ", e
+            print ("Something went wrong: ", e)
